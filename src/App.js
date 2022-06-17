43f249a4114c4470bcc6fbd57b5486fb6f102e64
gkgiran <gkdesign1995@gmail.com>
2022-06-18 01:35:03 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Dices from './components/dices';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='App-title'>Dice Game 🎲</h1>
+      </header>
+      <Dices />
+    </div>
+  );
+}
+
+export default App;
